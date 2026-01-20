7a83c65e3adc7244d67a84480a721e2aedc35bc3
pura55 <nekotonaito2@gmail.com>
2026-01-20 13:42:41 +0900

    エネミーの動作を変更

--- J/project/Source/Enemy.cpp
+++ J/project/Source/Enemy.cpp
@@ -22,6 +22,11 @@ Enemy::Enemy(float x, float y)
 	MovementsCount = 0;
 	DirChara = 3;
 	HitJudge = true;
+
+	//追加
+	avoidTimer = 0.0f;
+	state = MoveAxis::CHASE;
+	moveAxis = MoveAxis::X;
 }
 
 Enemy::Enemy(float x, float y, int waitFrame)
@@ -49,7 +54,6 @@ void Enemy::Update()
 
 	GameManager* gameManager = FindGameObject<GameManager>();
 	Player* player = FindGameObject<Player>();
-	VECTOR3 pPos = player->GetPosition();
 	Fader* fader = FindGameObject<Fader>();
 
 	if (player->IsHit(EnemyX, EnemyY))
@@ -59,110 +63,247 @@ void Enemy::Update()
 		SceneManager::ChangeScene("GAMEOVER");
 	}
 
-	if (fabs(pPos.x - EnemyX) > 5.0f && HitJudge)
+	////after////
+
+	VECTOR3 pPos = player->GetPosition();
+	MainMap* ObjectHit = FindGameObject<MainMap>();
+
+	float dx = pPos.x - EnemyX;
+	float dy = pPos.y - EnemyY;
+
+	float moveX = 0.0f;
+	float moveY = 0.0f;
+
+	//チェイス
+	if (state == MoveAxis::CHASE)
 	{
-		if (pPos.x > EnemyX)//px > exの時
+		//x軸とy軸の差を図る
+		if (fabs(dx) > fabs(dy))
+			moveAxis = MoveAxis::X;
+		else
+			moveAxis = MoveAxis::Y;
+
+		if (moveAxis == MoveAxis::X)
 		{
-			EnemyX += speed;//移動距離加算
-			DirChara = 1; //方向
+			moveX = (dx > 0) ? speed : -speed;
+		}
+		else
+		{
+			moveY = (dy > 0) ? speed : -speed;
+		}
 
-			MovementsCount += 1; //キャラクターの動作の処理
-			if (MovementsCount >=15) {
+		int push1_R = ObjectHit->HitCheckRight(EnemyX + 50 + moveX, EnemyY + 64);
+		int push2_R = ObjectHit->HitCheckRight(EnemyX + 50 + moveX, EnemyY + 95);
+		int push1_L = ObjectHit->HitCheckLeft(EnemyX + 14 + moveX, EnemyY + 64);
+		int push2_L = ObjectHit->HitCheckLeft(EnemyX + 14 + moveX, EnemyY + 95);
+		int push1_U = ObjectHit->HitCheckUp(EnemyX + 24, EnemyY + 64 + moveY);
+		int push2_U = ObjectHit->HitCheckUp(EnemyX + 50, EnemyY + 64 + moveY);
+		int push1_D = ObjectHit->HitCheckDown(EnemyX + 24, EnemyY + 95 + moveY);
+		int push2_D = ObjectHit->HitCheckDown(EnemyX + 50, EnemyY + 95 + moveY);
 
-				MovementsPattern = (MovementsPattern + 1) % 3 + 0;
 
-				MovementsCount = 0;
-			}
+		if (max(push1_R, push2_R) || max(push1_L, push2_L) || max(push1_U, push2_U) || max(push1_D, push2_D))
+		{
+			//回避へ遷移
+			state = MoveAxis::AVOID;
+			avoidTimer = 0.3f;
 
-			MainMap* ObjectHit = FindGameObject<MainMap>();
-			int push1 = ObjectHit->HitCheckRight(EnemyX + 50, EnemyY + 51);
-			int push2 = ObjectHit->HitCheckRight(EnemyX + 50, EnemyY + 63);
-			EnemyX -= max(push1, push2);
-			if (max(push1, push2) > 0)
+			//回避軸は追尾軸と逆
+			moveAxis = (moveAxis == MoveAxis::X) ? MoveAxis::Y : MoveAxis::X;
+
+			if (moveAxis == MoveAxis::X)
 			{
-				HitJudge = false;
+				moveX = (dx > 0) ? speed : -speed;
+			}
+			else
+			{
+				moveY = (dy > 0) ? speed : -speed;
 			}
 		}
-		else if
-			(pPos.x < EnemyX)//px < exの時
+	}
+	else//回避
+	{
+		avoidTimer -= Time::DeltaTime();
+
+		//回避中は軸を固定
+		if (moveAxis == MoveAxis::X)
 		{
-			EnemyX -= speed;
+			moveX = (dx > 0) ? speed : -speed;
+		}
+		else
+		{
+			moveY = (dy > 0) ? speed : -speed;
+		}
+
+		if (avoidTimer <= 0.0f)
+		{
+			state = MoveAxis::CHASE;
+		}
+	}
+
+	if (EnemyX != 0)
+	{
+		EnemyX += moveX;
+		if (moveX > 0)
+		{
+			DirChara = 1;
+		}
+		if (moveX < 0)
+		{
 			DirChara = 0;
-
-			MovementsCount += 1; //キャラクターの動作の処理
-			if (MovementsCount >= 15) {
-
-				MovementsPattern = (MovementsPattern + 1) % 3 + 0;
-
-				MovementsCount = 0;
-			}
-
-			MainMap* ObjectHit = FindGameObject<MainMap>();
-			int push1 = ObjectHit->HitCheckLeft(EnemyX + 14, EnemyY + 51);
-			int push2 = ObjectHit->HitCheckLeft(EnemyX + 14, EnemyY + 63);
-			EnemyX += max(push1, push2);
-			if (max(push1, push2) > 0)
-			{
-				HitJudge = false;
-			}
 		}
+
+		MovementsCount += 1; //キャラクターの動作の処理
+		if (MovementsCount >= 15) {
+
+			MovementsPattern = (MovementsPattern + 1) % 3 + 0;
+
+			MovementsCount = 0;
+		}
+
+		int push1_R = ObjectHit->HitCheckRight(EnemyX + 50, EnemyY + 64);
+		int push2_R = ObjectHit->HitCheckRight(EnemyX + 50, EnemyY + 95);
+		int push1_L = ObjectHit->HitCheckLeft(EnemyX + 14, EnemyY + 64);
+		int push2_L = ObjectHit->HitCheckLeft(EnemyX + 14, EnemyY + 95);
+		EnemyX -= max(push1_R, push2_R);
+		EnemyX += max(push1_L, push2_L);
 	}
-	else
+
+
+	if (EnemyY != 0)
 	{
-		if (pPos.y > EnemyY || !HitJudge)//py > eyの時
+		EnemyY += moveY;
+		if (moveY > 0)
 		{
-			EnemyY += speed;
 			DirChara = 3;
-
-			MovementsCount += 1; //キャラクターの動作の処理
-			if (MovementsCount >= 15) {
-
-				MovementsPattern = (MovementsPattern + 1) % 3 + 0;
-
-				MovementsCount = 0;
-			}
-
-			MainMap* ObjectHit = FindGameObject<MainMap>();
-			int push1 = ObjectHit->HitCheckDown(EnemyX + 14, EnemyY + 63);
-			int push2 = ObjectHit->HitCheckDown(EnemyX + 50, EnemyY + 63);
-			EnemyY -= max(push1, push2);
-
-			if (fabs(pPos.y - EnemyY) > 2.0f)
-			{
-				HitJudge = true;
-			}
-			if (max(push1, push2) > 0)
-			{
-				HitJudge = true;
-			}
 		}
-		else if (pPos.y < EnemyY)//py < eyの時
+		if (moveY < 0)
 		{
-			EnemyY -= speed;
 			DirChara = 2;
-
-			MovementsCount += 1; //キャラクターの動作の処理
-			if (MovementsCount >= 15) {
-
-				MovementsPattern = (MovementsPattern + 1) % 3 + 0;
-
-				MovementsCount = 0;
-			}
-
-			MainMap* ObjectHit = FindGameObject<MainMap>();
-			int push1 = ObjectHit->HitCheckUp(EnemyX + 14, EnemyY + 51);
-			int push2 = ObjectHit->HitCheckUp(EnemyX + 50, EnemyY + 51);
-			EnemyY += max(push1, push2);
-			if (fabs(pPos.y - EnemyY) > 2.0f)
-			{
-				HitJudge = true;
-			}
-			if (max(push1, push2) > 0)
-			{
-				HitJudge = true;
-			}
 		}
+
+		MovementsCount += 1; //キャラクターの動作の処理
+		if (MovementsCount >= 15) {
+
+			MovementsPattern = (MovementsPattern + 1) % 3 + 0;
+
+			MovementsCount = 0;
+		}
+
+		int push1_U = ObjectHit->HitCheckUp(EnemyX + 24, EnemyY + 64);
+		int push2_U = ObjectHit->HitCheckUp(EnemyX + 50, EnemyY + 64);
+		int push1_D = ObjectHit->HitCheckDown(EnemyX + 24, EnemyY + 95);
+		int push2_D = ObjectHit->HitCheckDown(EnemyX + 50, EnemyY + 95);
+		EnemyY -= max(push1_D, push2_D);
+		EnemyY += max(push1_U, push2_U);
 	}
+
+	////before////
+	
+	//if (fabs(pPos.x - EnemyX) > 5.0f && HitJudge)
+	//{
+	//	if (pPos.x > EnemyX)//px > exの時
+	//	{
+	//		EnemyX += speed;//移動距離加算
+	//		DirChara = 1; //方向
+
+	//		MovementsCount += 1; //キャラクターの動作の処理
+	//		if (MovementsCount >=15) {
+
+	//			MovementsPattern = (MovementsPattern + 1) % 3 + 0;
+
+	//			MovementsCount = 0;
+	//		}
+
+	//		MainMap* ObjectHit = FindGameObject<MainMap>();
+	//		int push1 = ObjectHit->HitCheckRight(EnemyX + 50, EnemyY + 51);
+	//		int push2 = ObjectHit->HitCheckRight(EnemyX + 50, EnemyY + 63);
+	//		EnemyX -= max(push1, push2);
+	//		if (max(push1, push2) > 0)
+	//		{
+	//			HitJudge = false;
+	//		}
+	//	}
+	//	else if
+	//		(pPos.x < EnemyX)//px < exの時
+	//	{
+	//		EnemyX -= speed;
+	//		DirChara = 0;
+
+	//		MovementsCount += 1; //キャラクターの動作の処理
+	//		if (MovementsCount >= 15) {
+
+	//			MovementsPattern = (MovementsPattern + 1) % 3 + 0;
+
+	//			MovementsCount = 0;
+	//		}
+
+	//		MainMap* ObjectHit = FindGameObject<MainMap>();
+	//		int push1 = ObjectHit->HitCheckLeft(EnemyX + 14, EnemyY + 51);
+	//		int push2 = ObjectHit->HitCheckLeft(EnemyX + 14, EnemyY + 63);
+	//		EnemyX += max(push1, push2);
+	//		if (max(push1, push2) > 0)
+	//		{
+	//			HitJudge = false;
+	//		}
+	//	}
+	//}
+	//else
+	//{
+	//	if (pPos.y > EnemyY || !HitJudge)//py > eyの時
+	//	{
+	//		EnemyY += speed;
+	//		DirChara = 3;
+
+	//		MovementsCount += 1; //キャラクターの動作の処理
+	//		if (MovementsCount >= 15) {
+
+	//			MovementsPattern = (MovementsPattern + 1) % 3 + 0;
+
+	//			MovementsCount = 0;
+	//		}
+
+	//		MainMap* ObjectHit = FindGameObject<MainMap>();
+	//		int push1 = ObjectHit->HitCheckDown(EnemyX + 14, EnemyY + 63);
+	//		int push2 = ObjectHit->HitCheckDown(EnemyX + 50, EnemyY + 63);
+	//		EnemyY -= max(push1, push2);
+
+	//		if (fabs(pPos.y - EnemyY) > 2.0f)
+	//		{
+	//			HitJudge = true;
+	//		}
+	//		if (max(push1, push2) > 0)
+	//		{
+	//			HitJudge = true;
+	//		}
+	//	}
+	//	else if (pPos.y < EnemyY)//py < eyの時
+	//	{
+	//		EnemyY -= speed;
+	//		DirChara = 2;
+
+	//		MovementsCount += 1; //キャラクターの動作の処理
+	//		if (MovementsCount >= 15) {
+
+	//			MovementsPattern = (MovementsPattern + 1) % 3 + 0;
+
+	//			MovementsCount = 0;
+	//		}
+
+	//		MainMap* ObjectHit = FindGameObject<MainMap>();
+	//		int push1 = ObjectHit->HitCheckUp(EnemyX + 14, EnemyY + 51);
+	//		int push2 = ObjectHit->HitCheckUp(EnemyX + 50, EnemyY + 51);
+	//		EnemyY += max(push1, push2);
+	//		if (fabs(pPos.y - EnemyY) > 2.0f)
+	//		{
+	//			HitJudge = true;
+	//		}
+	//		if (max(push1, push2) > 0)
+	//		{
+	//			HitJudge = true;
+	//		}
+	//	}
+	//}
 }
 
 void Enemy::Draw()
--- J/project/Source/Enemy.h
+++ J/project/Source/Enemy.h
@@ -1,6 +1,17 @@
 #pragma once
 #include "../Library/GameObject.h"
 
+enum class MoveAxis
+{
+	//移動軸
+	X,
+	Y,
+
+	//行動状態
+	CHASE,
+	AVOID
+
+};
 
 class Enemy :public GameObject 
 {
@@ -24,6 +35,11 @@ private:
 	int MovementsCount; //動作のカウント
 	int DirChara;
 	bool HitJudge;
+
+	//回避
+	MoveAxis  state;  //エネミーの状態
+	MoveAxis moveAxis;//移動軸
+	float avoidTimer; //回避時間
 
 	static bool followReserved;
 	static float followX;
